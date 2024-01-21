17d565a102881a2636c79f8a93614a9d4f59dfe9
Ruo Liang <liangruo410@gmail.com>
2024-01-21 13:39:27 +0800

    main throws IOException

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -11,7 +11,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: what if args is empty?
         if (args.length == 0){
             System.out.println("Please enter a command.");
@@ -22,7 +22,7 @@ public class Main {
             case "init":
                 // TODO: handle the `init` command
                 validateNumArgs(args, 1);
-//                Repository.setupPersistence();
+                Repository.setupPersistence();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
