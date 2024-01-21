c707471f9309b076d74b429c1af729ef1d6a5109
Ruo Liang <liangruo410@gmail.com>
2024-01-21 13:04:14 +0800

    commit constructor and saveCommit

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -30,11 +30,11 @@ public class Commit implements Serializable {
 
     /* TODO: fill in the rest of this class. */
     /** The timestamp of this Commit. */
-//    private String timestamp;
-//    /** The parent of this Commit. */
-//    private String parent;
-//    /** contains the file names and their blobs in the commit.*/
-//    public HashMap<String, String> filenameBlob;
+    private String timestamp;
+    /** The parent of this Commit. */
+    private String parent;
+    /** contains the file names and their blobs in the commit.*/
+    public HashMap<String, String> filenameBlob;
 
     /* TODO: fill in the rest of this class. */
     /** Constructor for Commit class.
@@ -42,30 +42,30 @@ public class Commit implements Serializable {
      * @param parent: the SHA - 1 hash of the parent commit.
      * @param timestamp: the timestamp of the commit.
      * */
-//    public Commit(String message, String parent, String timestamp){
-//        this.message = message;
-//        this.parent = parent;
-//        this.timestamp = timestamp;
-//        this.filenameBlob = new HashMap<>();
-//    }
+    public Commit(String message, String parent, String timestamp){
+        this.message = message;
+        this.parent = parent;
+        this.timestamp = timestamp;
+        this.filenameBlob = new HashMap<>();
+    }
 
     /** Write commit into a file whose name is the sha1 of the commit. */
-//    public void saveCommit(File saveCommitDIR) throws IOException {
-//        /**temporarily used for convert commit object to string for sha1*/
-//        File tempOutFile = Utils.join(saveCommitDIR, "tempOutFile");
-//        if (!tempOutFile.exists()) {
-//            tempOutFile.createNewFile();
-//        }
-//        writeObject(tempOutFile, this);
-//        String commitString = Utils.readContentsAsString(tempOutFile);
-//        String commitSha1 = Utils.sha1(commitString);
-//        File outFile = Utils.join(saveCommitDIR, commitSha1);
-//        if (!outFile.exists()){
-//            outFile.createNewFile();
-//            writeObject(outFile, this); // the saved commit object
-//        }
+    public void saveCommit(File saveCommitDIR) throws IOException {
+        /**temporarily used for convert commit object to string for sha1*/
+        File tempOutFile = Utils.join(saveCommitDIR, "tempOutFile");
+        if (!tempOutFile.exists()) {
+            tempOutFile.createNewFile();
+        }
+        writeObject(tempOutFile, this);
+        String commitString = Utils.readContentsAsString(tempOutFile);
+        String commitSha1 = Utils.sha1(commitString);
+        File outFile = Utils.join(saveCommitDIR, commitSha1);
+        if (!outFile.exists()){
+            outFile.createNewFile();
+            writeObject(outFile, this); // the saved commit object
+        }
 
 //        updateHEAD(commitSha1); //after updateHEAD and updateMaster added, this two lines need to be added.
 //        updateMaster(commitSha1);
-//    }
+    }
 }
